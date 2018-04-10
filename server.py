694fd2a48a2c574e4edcb6a0f8298be616e18c18
Justin Woodbridge <me@justinwoodbridge.com>
2018-04-09 22:06:10 -0400

    add basic ad table on index page

--- server.py
+++ server.py
@@ -118,43 +118,23 @@ def login_required(fn):
 def index():
   cursor = g.conn.execute("SELECT name FROM library")
   libraries = []
+
   for result in cursor:
     libraries.append(result['name'].lower())  # can also be accessed using result[0]
+
   cursor.close()
 
-  #
-  # Flask uses Jinja templates, which is an extension to HTML where you can
-  # pass data to a template and dynamically generate HTML based on the data
-  # (you can think of it as simple PHP)
-  # documentation: https://realpython.com/blog/python/primer-on-jinja-templating/
-  #
-  # You can see an example template in templates/index.html
-  #
-  # context are the variables that are passed to the template.
-  # for example, "data" key in the context variable defined below will be 
-  # accessible as a variable in index.html:
-  #
-  #     # will print: [u'grace hopper', u'alan turing', u'ada lovelace']
-  #     <div>{{data}}</div>
-  #     
-  #     # creates a <div> tag for each element in data
-  #     # will print: 
-  #     #
-  #     #   <div>grace hopper</div>
-  #     #   <div>alan turing</div>
-  #     #   <div>ada lovelace</div>
-  #     #
-  #     {% for n in data %}
-  #     <div>{{n}}</div>
-  #     {% endfor %}
-  #
-  context = dict(data = libraries, user = g.user)
+  r = g.conn.execute("SELECT a.text, so.library_name, so.seat_id FROM ads a, seat_offerings so WHERE so.seat_offering_id = a.seat_offering_id")  
 
+  ads = []
+  for row in r:
+    # TODO: We could parse these from tuples into dicts with named attribute keys but fuck it.
+    ads.append(r.fetchone())  
 
-  #
-  # render_template looks in the templates/ folder for files.
-  # for example, the below file reads template/index.html
-  #
+  print(ads)
+
+  context = dict(data = libraries, user = g.user, ads=ads)
+
   return render_template("index.html", **context)
 
 
